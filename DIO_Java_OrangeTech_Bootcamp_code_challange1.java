e69ff658d730f24b7bc9e01b893aba1f15498597
Bruno Gusmão <92750091+BrunoDev2003@users.noreply.github.com>
2023-06-30 20:55:24 -0300

    Create DIO_Java_OrangeTech_Bootcamp_code_challange1.java

---
+++ DIO_Java_OrangeTech_Bootcamp_code_challange1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; 
+    
+public class Program {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int velocidadeAtual = leitor.nextInt();
+        
+        // TODO: Crie a condição necessária para verificar se o motorista, de acordo com a entrada, foi multado ou não.
+        
+        
+      String result = (velocidadeAtual > 60) ? ("Foi multado") : ("Nao foi multado");
+      System.out.println(result);
+
+    }
+}
